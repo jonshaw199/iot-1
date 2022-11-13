5da002fcedc4524559e9c170cd4fa3c36db8d970
Jon Shaw <jonshaw199@gmail.com>
2022-11-13 01:15:06 -0800

    Types

--- server/controllers/org.ts
+++ server/controllers/org.ts
@@ -1,14 +1,16 @@
+import { Response } from "express";
 import orgModel from "../models/org";
+import { Request } from "../types";
 
 // list all orgs
-export const index = (req, res) => {
+export const index = (req: Request, res: Response) => {
   orgModel.find({}, (err, orgs) => {
     res.json(orgs);
   });
 };
 
 // create a new message
-export const create = (req, res) => {
+export const create = (req: Request, res: Response) => {
   orgModel.create(req.body, (err, org) => {
     if (err) return res.json({ success: false, code: err.code });
     res.json({
