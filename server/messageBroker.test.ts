771326adb6132e49d9d6695c713d8553cba6897d
Jon Shaw <jonshaw199@gmail.com>
2022-11-17 19:10:29 -0800

    jest

--- server/messageBroker.test.ts
+++ server/messageBroker.test.ts
@@ -26,3 +26,15 @@ test("wildcard-multi", () => {
   expect(MessageBroker.getSubscribers("home/lights/kitchen").size).toBe(1);
   MessageBroker.clearTopicTree();
 });
+
+test("all", () => {
+  MessageBroker.subscribe(1, "home/#");
+  MessageBroker.subscribe(1, "office/alarm/*");
+  MessageBroker.subscribe(2, "home/#");
+  MessageBroker.subscribe(3, "*");
+  MessageBroker.unsubscribe(3, "*");
+  expect(MessageBroker.getSubscribers("home/lights").size).toBe(2);
+  expect(MessageBroker.getSubscribers("office/alarm/front-door").size).toBe(1);
+  expect(MessageBroker.getSubscribers("home/lights").size).toBe(2);
+  MessageBroker.clearTopicTree();
+});
