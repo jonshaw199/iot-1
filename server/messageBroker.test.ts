a53ddb9d84fc53c4a714608d6b1c2c6d5c8ad7c6
Jon Shaw <jonshaw199@gmail.com>
2022-11-17 17:23:53 -0800

    jest

---
+++ server/messageBroker.test.ts
@@ -0,0 +1,14 @@
+import { expect, test } from "@jest/globals";
+
+import MessageBroker from "./messageBroker";
+
+test("subscribe", () => {
+  MessageBroker.subscribe(1, "home/lights");
+  expect(MessageBroker.getSubscribers("home/lights").size).toBe(1);
+});
+
+test("unsubscribe", () => {
+  MessageBroker.subscribe(1, "home/lights");
+  MessageBroker.unsubscribe(1, "home/lights");
+  expect(MessageBroker.getSubscribers("home/lights").size).toBe(0);
+});
