70c9208bd9aa44abef15740cbb40bffd0577653a
Jon Shaw <jonshaw199@gmail.com>
2022-11-20 08:40:07 -0800

    Seed DB

--- server/models/org.ts
+++ server/models/org.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 import { Org } from "../types";
 
-const orgSchema = new mongoose.Schema<Org>();
+const orgSchema = new mongoose.Schema<Org>({
+  name: { type: String },
+});
 
 const orgModel = mongoose.model<Org>("Org", orgSchema);
 
---
+++ server/seed.ts
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import deviceModel from "./models/device";
+import orgModel from "./models/org";
+import userModel from "./models/user";
+import { Board } from "./types";
+import messageModel from "./models/message";
+
+async function connect() {
+  try {
+    return await mongoose.connect(process.env.MONGODB_URI);
+  } catch (e) {
+    console.log(`Failed to connect to MongoDB: ${e}`);
+  }
+}
+
+async function deleteAllDocuments() {
+  try {
+    await Promise.all([
+      userModel.deleteMany({}),
+      deviceModel.deleteMany({}),
+      messageModel.deleteMany({}),
+    ]);
+    return await orgModel.deleteMany({});
+  } catch (e) {
+    console.log(`Failed to delete all documents before seeding: ${e}`);
+  }
+}
+
+async function insertDocuments() {
+  const org = new orgModel({ name: "root" });
+
+  const devices = [
+    new deviceModel({
+      board: Board.BOARD_OTHER,
+      name: "webclient",
+      orgId: org.id,
+    }),
+  ];
+
+  const users = [
+    new userModel({
+      email: process.env.ROOT_EMAIL,
+      password: process.env.ROOT_PASS,
+      orgId: org.id,
+    }),
+  ];
+
+  try {
+    await org.save();
+    return Promise.all([
+      ...devices.map((d) => d.save()),
+      ...users.map((u) => u.save()),
+    ]);
+  } catch (e) {
+    console.log(`Failed to seed DB with new documents: ${e}`);
+  }
+}
+
+async function seed() {
+  if (await connect()) {
+    try {
+      if (await deleteAllDocuments()) {
+        await insertDocuments();
+        console.log("Seed completed successfully");
+      }
+    } finally {
+      mongoose.disconnect();
+    }
+  }
+}
+
+seed();
