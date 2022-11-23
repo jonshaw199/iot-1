b2209d473b0e0db599d245973f70db3d233b0e60
Jon Shaw <jonshaw199@gmail.com>
2022-11-23 00:09:27 -0800

    Fixing tests

--- server/messageBroker.test.ts
+++ server/messageBroker.test.ts
@@ -4,26 +4,26 @@ import MessageBroker from "./messageBroker";
 
 test("subscribe", () => {
   MessageBroker.subscribe(1, "home/lights");
-  expect(MessageBroker.getSubscribers("home/lights").size).toBe(1);
+  expect(MessageBroker.getSubscriberIDs("home/lights").size).toBe(1);
   MessageBroker.clearTopicTree();
 });
 
 test("unsubscribe", () => {
   MessageBroker.subscribe(1, "home/lights");
   MessageBroker.unsubscribe(1, "home/lights");
-  expect(MessageBroker.getSubscribers("home/lights").size).toBe(0);
+  expect(MessageBroker.getSubscriberIDs("home/lights").size).toBe(0);
   MessageBroker.clearTopicTree();
 });
 
 test("wildcard", () => {
   MessageBroker.subscribe(1, "home/*");
-  expect(MessageBroker.getSubscribers("home/lights").size).toBe(1);
+  expect(MessageBroker.getSubscriberIDs("home/lights").size).toBe(1);
   MessageBroker.clearTopicTree();
 });
 
 test("wildcard-multi", () => {
   MessageBroker.subscribe(1, "home/#");
-  expect(MessageBroker.getSubscribers("home/lights/kitchen").size).toBe(1);
+  expect(MessageBroker.getSubscriberIDs("home/lights/kitchen").size).toBe(1);
   MessageBroker.clearTopicTree();
 });
 
@@ -33,8 +33,10 @@ test("all", () => {
   MessageBroker.subscribe(2, "home/#");
   MessageBroker.subscribe(3, "*");
   MessageBroker.unsubscribe(3, "*");
-  expect(MessageBroker.getSubscribers("home/lights").size).toBe(2);
-  expect(MessageBroker.getSubscribers("office/alarm/front-door").size).toBe(1);
-  expect(MessageBroker.getSubscribers("home/lights").size).toBe(2);
+  expect(MessageBroker.getSubscriberIDs("home/lights").size).toBe(2);
+  expect(MessageBroker.getSubscriberIDs("office/alarm/front-door").size).toBe(
+    1
+  );
+  expect(MessageBroker.getSubscriberIDs("home/lights").size).toBe(2);
   MessageBroker.clearTopicTree();
 });
