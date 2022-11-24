851befa8245b90bdd2285a2351c25255350bc934
Jon Shaw <jonshaw199@gmail.com>
2022-11-24 11:53:25 -0800

    Improved color picker

--- web/src/components/ColorPicker.tsx
+++ web/src/components/ColorPicker.tsx
@@ -1,27 +1,44 @@
 import iro from "@jaames/iro";
 import { IroColorPicker } from "@jaames/iro/dist/ColorPicker";
-import { useRef, useEffect } from "react";
-import { Nullable } from "../serverTypes";
+import { useRef, useEffect, useCallback } from "react";
+import _throttle from "lodash/throttle";
 
 export default function ColorPicker({
   initial,
+  throttle,
   onChange = () => null,
+  onChangeThrottled = () => null,
+  onEnd = () => null,
 }: {
-  initial?: Nullable<iro.Color>;
+  initial?: iro.Color | null;
+  throttle?: number | null;
   onChange?: (c: iro.Color) => void;
+  onChangeThrottled?: (c: iro.Color) => void;
+  onEnd?: (c: iro.Color) => void;
 }) {
   const pickerDivRef = useRef(null);
   const pickerRef = useRef<IroColorPicker | null>(null);
+
+  const throttled = useCallback(
+    (c: iro.Color) =>
+      throttle && _throttle(() => onChangeThrottled(c), throttle),
+    [throttle, onChangeThrottled]
+  );
 
   useEffect(() => {
     if (!pickerRef.current) {
       pickerRef.current = iro.ColorPicker(pickerDivRef.current!, {
         color: initial || undefined,
       });
+      pickerRef.current.on("color:change", (c: iro.Color) => {
+        onChange(c);
+        throttled(c);
+      });
+      pickerRef.current.on("input:end", (c: iro.Color) => {
+        console.log(c);
+        onEnd(c);
+      });
     }
-    pickerRef.current.on("color:change", (c: iro.Color) => {
-      onChange(c);
-    });
   }, [initial, onChange]);
 
   return <div ref={pickerDivRef} />;
