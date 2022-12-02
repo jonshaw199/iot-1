07ab9b5a4e4b33bc9c56a05abe3a95ed7d065198
Jon Shaw <jonshaw199@gmail.com>
2022-12-02 12:03:35 -0800

    Revert "Fire event even if unchanged; to revisit"
    This reverts commit 88e1c714f6c80418e4103696444fe8c9161d000c.

--- web/src/components/Lights.tsx
+++ web/src/components/Lights.tsx
@@ -62,11 +62,13 @@ export default function Lights() {
           <TextField
             label="State"
             value={selectedState}
-            SelectProps={{
-              onClose: () => changeState(selectedState),
+            onChange={(e) => {
+              const s = parseInt(e.target.value);
+              setSelectedState(s);
+              changeState(s);
             }}
-            onChange={(e) => setSelectedState(parseInt(e.target.value))}
             select
+            // sx={{ flex: 1 }}
           >
             {stateOptions.map(([value, name]: [number, string], i) => (
               <MenuItem value={value} key={i}>
