26b36b0357f3cc6dc2ae9431cf3de2bd1909852d
Jon Shaw <jonshaw199@gmail.com>
2022-12-01 20:59:11 -0800

    State change selector

--- web/src/components/Lights.tsx
+++ web/src/components/Lights.tsx
@@ -1,8 +1,14 @@
 import iro from "@jaames/iro";
-import { Paper, Box, useTheme } from "@mui/material";
-import { useContext, useCallback } from "react";
+import { Paper, Box, useTheme, TextField, MenuItem } from "@mui/material";
+import { useContext, useCallback, useState, useMemo } from "react";
 import { GlobalWebsocketContext } from "../hooks/useWebsocket";
-import { MessageType, Topics, PacketLightsColor } from "../serverTypes";
+import {
+  MessageType,
+  Topics,
+  PacketLightsColor,
+  Packet,
+  State,
+} from "../serverTypes";
 import { sendMessageThunk } from "../state/messageSlice";
 import { useDispatch } from "../state/store";
 import ColorPicker from "./ColorPicker";
@@ -11,6 +17,7 @@ export default function Lights() {
   const theme = useTheme();
   const ws = useContext(GlobalWebsocketContext);
   const dispatch = useDispatch();
+  const [selectedState, setSelectedState] = useState(0);
 
   const submitColor = useCallback(
     (c: iro.Color) => {
@@ -28,6 +35,32 @@ export default function Lights() {
     [dispatch, ws]
   );
 
+  const changeState = useCallback(
+    (s: number) => {
+      const msg: Packet = {
+        senderId: process.env.REACT_APP_DEVICE_ID || "",
+        type: MessageType.TYPE_MQTT_PUBLISH,
+        topic: Topics.LIGHTS_STATE,
+        state: s,
+        qos: 2,
+      };
+      dispatch(sendMessageThunk({ msg, ws }));
+    },
+    [dispatch, ws]
+  );
+
+  const stateOptions: [number, string][] = useMemo(
+    () => [
+      [State.STATE_HOME, "Home"],
+      [State.STATE_PATTERN_NOISE, "Pattern - Noise"],
+      [State.STATE_PATTERN_NOISEPLUSPALETTE, "Pattern - Noise+Palette"],
+      [State.STATE_PATTERN_TWINKLEFOX, "Pattern - Twinklefox"],
+      [State.STATE_RESTART, "Restart"],
+      [State.STATE_OTA, "OTA"],
+    ],
+    []
+  );
+
   return (
     <Paper>
       <Box
@@ -38,6 +71,23 @@ export default function Lights() {
         alignItems="center"
       >
         <ColorPicker throttle={333} onChangeThrottled={submitColor} />
+        <TextField
+          label="State"
+          value={selectedState}
+          onChange={(e) => {
+            const s = parseInt(e.target.value);
+            setSelectedState(s);
+            changeState(s);
+          }}
+          select
+          sx={{ minWidth: 250 }}
+        >
+          {stateOptions.map(([value, name]: [number, string], i) => (
+            <MenuItem value={value} key={i}>
+              {name}
+            </MenuItem>
+          ))}
+        </TextField>
       </Box>
     </Paper>
   );
