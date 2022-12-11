ba8335f2cf2503ad1f2bb55c45bb8b6699a25c9a
Jon Shaw <jonshaw199@gmail.com>
2022-12-11 08:44:59 -0800

    Redundant

--- firmware/lights/src/pattern/pattern.cpp
+++ firmware/lights/src/pattern/pattern.cpp
@@ -97,10 +97,10 @@ void Pattern::cbPattern()
 
 void Pattern::beatwave()
 {
-  uint8_t wave1 = beatsin8(9, 0, 255); // That's the same as beatsin8(9);
-  uint8_t wave2 = beatsin8(8, 0, 255);
-  uint8_t wave3 = beatsin8(7, 0, 255);
-  uint8_t wave4 = beatsin8(6, 0, 255);
+  uint8_t wave1 = beatsin8(9);
+  uint8_t wave2 = beatsin8(8);
+  uint8_t wave3 = beatsin8(7);
+  uint8_t wave4 = beatsin8(6);
 
   for (int i = 0; i < CNT; i++)
   {
