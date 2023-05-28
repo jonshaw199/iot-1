6392877b280914442ef1d43c1ff0ffb656d32e50
jon <jonshaw199@gmail.com>
2023-05-28 14:14:34 -0700

    lights devcontainer; encrypt global build flags

--- firmware/lights/.devcontainer/postCreate.sh
+++ firmware/lights/.devcontainer/postCreate.sh
@@ -1,15 +1,15 @@
-sudo apt install git
-
-if ! [ -e ../.env ]
+if ! [ -e ../buildFlags.h ]
 then
-    read -p "Decrypt files? (Y/N): " confirm
+    read -p "Decrypt build flags file? (Y/N): " confirm
     if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+        curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+        sudo apt-get install -y nodejs git
         git clone https://github.com/jonshaw199/crypt.git
         read -p "Enter encryption key: " key
         read -p "Enter initialization vector: " iv
-        node crypt/index.js -d . platformio.ini.encrypted "$key" "$iv"
+        node crypt/index.js -d . buildFlags.h.encrypted "$key" "$iv"
         rm -rf crypt
-        mv platformio.ini ..
+        mv buildFlags.h ..
     fi
 fi
 
