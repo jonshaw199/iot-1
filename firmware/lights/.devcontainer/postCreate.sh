e54e2f931bdd32b22d997c7d9ab71e55a9adfb14
jon <jonshaw199@gmail.com>
2023-05-28 22:17:08 -0700

    Use bash

--- firmware/lights/.devcontainer/postCreate.sh
+++ firmware/lights/.devcontainer/postCreate.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 if ! [ -e ../buildFlags.h ]
 then
-    read -p "Decrypt build flags file? (Y/N): " confirm
-    if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
-        curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-        sudo apt-get install -y nodejs git
+    read -r -p "Decrypt build flags file? (Y/N): " response
+    response=${response,,}    # tolower
+    if [[ "$response" =~ ^(yes|y)$ ]]
+    then
         git clone https://github.com/jonshaw199/crypt.git
         read -p "Enter encryption key: " key
         read -p "Enter initialization vector: " iv
